ca73f1e638be868e46393b00633799bacce12ef1
oussemabes <oussema.besbes@supcom.tn>
2023-12-03 04:38:39 +0100

    Initial commit

---
+++ src/graphql/mutations.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const CreatePost = /* GraphQL */ `
+  mutation CreatePost($title: String!, $author: String!) {
+    CreatePost(title: $title, author: $author) {
+      id
+      title
+      author
+      __typename
+    }
+  }
+`;
---
+++ src/graphql/queries.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPost = /* GraphQL */ `
+  query GetPost($id: ID!) {
+    getPost(id: $id) {
+      id
+      title
+      author
+      __typename
+    }
+  }
+`;
+export const listPosts = /* GraphQL */ `
+  query ListPosts {
+    listPosts {
+      id
+      title
+      author
+      __typename
+    }
+  }
+`;
