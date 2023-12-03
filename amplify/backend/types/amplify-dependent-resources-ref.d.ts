8ab83e4a74fbd2f80ad1a0fc175e553a75a0c34d
oussemabes <oussema.besbes@supcom.tn>
2023-12-03 13:49:07 +0100

    four

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "devopschallenge": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
